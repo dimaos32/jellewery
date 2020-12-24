7a7cad024ca45eca151579b41d1f24721d6d71b7
Dima Os <dimaos@protonmail.com>
2020-12-24 13:39:37 +0300

    Выполняет меню для планшет. версии

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('Big burger');
+(function () {
+
+  var header = document.querySelector('.header');
+  var menuToggle = header.querySelector('.header__menu-toggle');
+  var navigation = header.querySelector('.navigation');
+
+  header.classList.add('header--menu-closed');
+  menuToggle.classList.add('header__menu-toggle--closed');
+  navigation.classList.add('navigation--closed');
+
+  var onToggleMenuClick = function () {
+    header.classList.toggle('header--menu-opened');
+    header.classList.toggle('header--menu-closed');
+    menuToggle.classList.toggle('header__menu-toggle--opened');
+    menuToggle.classList.toggle('header__menu-toggle--closed');
+    navigation.classList.toggle('navigation--opened');
+    navigation.classList.toggle('navigation--closed');
+  };
+
+  menuToggle.addEventListener('click', onToggleMenuClick);
+})();
