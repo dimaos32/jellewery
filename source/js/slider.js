4729da9517e6e7af82a71cc3095de3403ff6f0f2
Dima Os <dimaos@protonmail.com>
2020-12-27 20:25:35 +0300

    Добавляет смену слайдов по свайпу

--- source/js/slider.js
+++ source/js/slider.js
@@ -12,6 +12,8 @@
   var slidesQuantity = 3;
   var currentSlide = 0;
 
+  var fingerDounX = null;
+
   var sliderFrame = document.querySelector('.slider__wrapper');
   var slider = sliderFrame.querySelector('.slider__list');
   var products = slider.querySelectorAll('.slider__list li');
@@ -21,10 +23,14 @@
   var paginationBtns = pagination.querySelectorAll('.slider__control-pagination');
   var currentPaginationBtn = pagination.querySelector('.slider__control-pagination--current');
 
+  var currentSliderFrameWidth = sliderFrame.offsetWidth;
+
+  var isMobile = currentSliderFrameWidth === SLIDER_WIDTH_ON_MOBILE;
+
   var getProductsPerFrame = function () {
-    if (sliderFrame.offsetWidth === SLIDER_WIDTH_ON_DESKTOP_MAX) {
+    if (currentSliderFrameWidth === SLIDER_WIDTH_ON_DESKTOP_MAX) {
       return PRODUCTS_PER_SLIDE_ON_DESKTOP_MAX;
-    } else if (sliderFrame.offsetWidth === SLIDER_WIDTH_ON_DESKTOP_MIN) {
+    } else if (currentSliderFrameWidth === SLIDER_WIDTH_ON_DESKTOP_MIN) {
       return PRODUCTS_PER_SLIDE_ON_DESKTOP_MIN;
     }
 
@@ -34,46 +40,47 @@
   var renderPagination = function (Q) {
     var fragment = document.createDocumentFragment();
 
-    pagination.innerHTML = '';
+    if (isMobile) {
+      var pagesElement = document.createElement('li');
+      var currentPageElement = document.createElement('span');
+      currentPageElement.textContent = currentSlide + 1;
+      pagesElement.textContent = ' of ' + slidesQuantity;
+      pagesElement.insertAdjacentElement('afterbegin', currentPageElement);
+      fragment.append(pagesElement);
+    } else {
+      for (var i = 0; i < Q; i++) {
+        var pageElement = document.createElement('li');
+        var pageBtn = document.createElement('button');
+        pageBtn.textContent = i + 1;
+        pageBtn.dataset.page = i;
+        pageBtn.classList.add('slider__control-pagination');
 
-    for (var i = 0; i < Q; i++) {
-      var pageElement = document.createElement('li');
-      var pageBtn = document.createElement('button');
-      pageBtn.textContent = i + 1;
-      pageBtn.dataset.page = i;
-      pageBtn.classList.add('slider__control-pagination');
+        if (i === 0) {
+          pageBtn.classList.add('slider__control-pagination--current');
+        }
 
-      if (i === 0) {
-        pageBtn.classList.add('slider__control-pagination--current');
+        pageElement.append(pageBtn);
+        fragment.append(pageElement);
       }
 
-      pageElement.append(pageBtn);
-      fragment.append(pageElement);
+      paginationBtns = pagination.querySelectorAll('.slider__control-pagination');
     }
 
     pagination.append(fragment);
-    paginationBtns = pagination.querySelectorAll('.slider__control-pagination');
   };
 
   var renderSliderControls = function () {
     slidesQuantity = Math.ceil(products.length / getProductsPerFrame());
+    isMobile = currentSliderFrameWidth === SLIDER_WIDTH_ON_MOBILE;
+    currentSlide = 0;
     slider.style.transform = 'translate(0px)';
 
     previewSlideBtn.classList.remove('slider__control-arrow--nojs');
     nextSlideBtn.classList.remove('slider__control-arrow--nojs');
 
+    pagination.innerHTML = '';
+
     renderPagination(slidesQuantity);
-  };
-
-  var changeSlide = function (element) {
-    var shift = sliderFrame.offsetWidth * element.dataset.page;
-
-    currentPaginationBtn = pagination.querySelector('.slider__control-pagination--current');
-
-    currentPaginationBtn.classList.remove('slider__control-pagination--current');
-    element.classList.add('slider__control-pagination--current');
-
-    slider.style.transform = 'translate(-' + shift + 'px)';
   };
 
   var getTargetPaginationBtn = function () {
@@ -82,7 +89,30 @@
     });
   };
 
-  var onPreviewSlideBtnClick = function () {
+  var changeSlide = function () {
+    var shift = currentSliderFrameWidth * currentSlide;
+
+    if (isMobile) {
+      var currentPaginationPage = pagination.querySelector('span');
+
+      currentPaginationPage.textContent = currentSlide + 1;
+    } else {
+      paginationBtns = pagination.querySelectorAll('.slider__control-pagination');
+
+      var target = getTargetPaginationBtn();
+
+      currentPaginationBtn = pagination.querySelector('.slider__control-pagination--current');
+
+      currentPaginationBtn.classList.remove('slider__control-pagination--current');
+      target.classList.add('slider__control-pagination--current');
+    }
+
+    slider.style.transform = 'translate(-' + shift + 'px)';
+  };
+
+  var switchPreviewSlide = function () {
+    paginationBtns = pagination.querySelectorAll('.slider__control-pagination');
+
     var target = getTargetPaginationBtn();
 
     if (currentSlide === 0) {
@@ -94,7 +124,9 @@
     changeSlide(target);
   };
 
-  var onNextSlideBtnClick = function () {
+  var switchNextSlide = function () {
+    paginationBtns = pagination.querySelectorAll('.slider__control-pagination');
+
     var target = getTargetPaginationBtn();
 
     currentSlide++;
@@ -104,6 +136,42 @@
     }
 
     changeSlide(target);
+  };
+
+  var onWindowResize = function () {
+    if (currentSliderFrameWidth !== sliderFrame.offsetWidth) {
+      currentSliderFrameWidth = sliderFrame.offsetWidth;
+      renderSliderControls();
+    }
+  };
+
+  var onPreviewSlideBtnClick = function () {
+    switchPreviewSlide();
+  };
+
+  var onNextSlideBtnClick = function () {
+    switchNextSlide();
+  };
+
+  var onTouchStart = function (evt) {
+    fingerDounX = evt.touches[0].clientX;
+  };
+
+  var onTouchMove = function (evt) {
+    if (!fingerDounX) {
+      return;
+    }
+
+    var fingerUpX = evt.touches[0].clientX;
+    var swipeLengthX = fingerDounX - fingerUpX;
+
+    if (swipeLengthX > 0) {
+      switchPreviewSlide();
+    } else {
+      switchNextSlide();
+    }
+
+    fingerDounX = null;
   };
 
   var onPaginationClick = function (evt) {
@@ -119,9 +187,13 @@
 
   renderSliderControls();
 
-  window.addEventListener('resize', renderSliderControls);
+  window.addEventListener('resize', onWindowResize);
 
   previewSlideBtn.addEventListener('click', onPreviewSlideBtnClick);
   nextSlideBtn.addEventListener('click', onNextSlideBtnClick);
+
+  slider.addEventListener('touchstart', onTouchStart, false);
+  slider.addEventListener('touchmove', onTouchMove, false);
+
   pagination.addEventListener('click', onPaginationClick);
 })();
