10d42406266dfe8a710a00f0165331b8f8072114
Dima Os <dimaos@protonmail.com>
2020-12-30 19:34:24 +0300

    Выполняет фильтр для планшет версии

--- source/js/catalog-filter.js
+++ source/js/catalog-filter.js
@@ -2,7 +2,9 @@
 
 (function () {
   if (document.querySelector('.catalog__filter')) {
-    var catalogFilter = document.querySelector('.catalog__filter');
+    var catalogFilter = document.querySelector('.catalog__filter-overlay');
+    var catalogFilterOpenBtn = document.querySelector('.catalog__filter-open-btn');
+    var catalogFiltercloseBtn = document.querySelector('.catalog__filter-close-btn');
 
     var onCatalogFilterClick = function (evt) {
       var target = evt.target.closest('.catalog__filter-fildset');
@@ -15,6 +17,23 @@
       }
     };
 
+    var oncatalogFilterOpenBtnClick = function (evt) {
+      evt.preventDefault();
+
+      catalogFilter.classList.add('catalog__filter-overlay--opened');
+      catalogFilter.classList.remove('catalog__filter-overlay--closed');
+
+      catalogFiltercloseBtn.addEventListener('click', oncatalogFiltercloseBtnClick);
+    };
+
+    var oncatalogFiltercloseBtnClick = function () {
+      catalogFilter.classList.remove('catalog__filter-overlay--opened');
+      catalogFilter.classList.add('catalog__filter-overlay--closed');
+
+      catalogFiltercloseBtn.removeEventListener('click', oncatalogFiltercloseBtnClick);
+    };
+
     catalogFilter.addEventListener('click', onCatalogFilterClick);
+    catalogFilterOpenBtn.addEventListener('click', oncatalogFilterOpenBtnClick);
   }
 })();
