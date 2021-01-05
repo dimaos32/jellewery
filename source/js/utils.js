c9ef23c6b51a6c6c2f251ffde508b786748c348f
Dima Os <dimaos@protonmail.com>
2021-01-05 16:56:29 +0300

    Предотвращает скролл при открытом мод. окне

--- source/js/utils.js
+++ source/js/utils.js
@@ -1,7 +1,16 @@
 'use strict';
 
 (function () {
+  var HTML = document.querySelector('html');
+
   var openModal = function (modal) {
+    if (document.body.offsetHeight > window.innerHeight) {
+      document.body.dataset.scrollY = self.pageYOffset;
+      document.body.style.top = document.body.dataset.scrollY * -1 + 'px';
+
+      document.body.classList.add('page--block-scroll');
+    }
+
     modal.classList.remove('modal--closed');
     modal.classList.add('modal--opened');
   };
@@ -9,6 +18,15 @@
   var closeModal = function (modal) {
     modal.classList.add('modal--closed');
     modal.classList.remove('modal--opened');
+
+    if (document.body.offsetHeight > window.innerHeight) {
+      document.body.classList.remove('page--block-scroll');
+
+      document.body.style.top = 0;
+      HTML.style.scrollBehavior = 'auto';
+      window.scrollTo(0, document.body.dataset.scrollY);
+      HTML.style.scrollBehavior = 'smooth';
+    }
   };
 
   window.utils = {
